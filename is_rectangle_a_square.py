b868e0c04e57b759bbf93a53f598b580cf4b2a11
Luke Beausoleil <luke.beausoleil@mths.ca>
2021-05-03 23:17:32 +0000

    added program that checks if rectangle is a square

---
+++ is_rectangle_a_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Luke Beausoleil
+# Created on: May 2021
+# This program checks if a rectangle is a square based off
+#   the length and width inputs from the user
+
+
+def main():
+
+    # input
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+
+    # process & output
+    if length == width:
+        print("\nThis rectangle is a square.")
+    else:
+        print("\nThis rectangle is not a square.")
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
